dc2408bc8ebf029d0795bdaad864537fc1e92c79
apon <mdaponahmed412@gmail.com>
2024-05-07 10:38:55 +0600

    simple count app

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+    let count = 1;
+    let color = "black";
+    const add = () => {
+        count = count++;
+        color = "blue";
+    };
+</script>
+
+<main>
+    <p>{count}</p>
+    <p>{color}</p>
+    <button on:click={add}> click </button>
+</main>
